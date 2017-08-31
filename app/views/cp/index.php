8988fd3c8cc557308efb4fc4e6052389be51b115
Ramin <missa@chmail.ir>
2017-08-31 09:25:34 +0430

    new

--- app/views/cp/index.php
+++ app/views/cp/index.php
@@ -8,11 +8,11 @@
 <div id='{{x.id}}' class="col-sm-3 col-xs-6 w3-padding-16" ng-repeat="x in names">
 
     <a href="<?=URL?>item/{{x.id}}"><div class="w3-round  w3-card-2 w3-center mycard w3-white" style="padding:8px" >
-<div align="right" class="">id:{{x.id}}</div>
+<div align="center" class="">{{x.name}}</div>
 <div class="w3-white" style="padding-bottom:0px;padding-left:10px;padding-right:10px">
 <img src="<?= URL ?>public/upload/{{x.card_image}}" style="width:100%;">
 <p align="right" class="font" style="padding:0px">
-{{x.name}}
+
 </p>
 <a class="btn btn-info" href="<?= URL ?>cp/delete_item/{{x.id}}">حذف</a>
 <a class="btn btn-success" href="<?= URL ?>edit_item/{{x.id}}">ویرایش</a>
